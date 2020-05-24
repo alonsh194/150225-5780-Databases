3f2d9a8b97b70f0bebb26011d1bd39277c71d707
alonsh194 <57595373+alonsh194@users.noreply.github.com>
2020-05-24 19:03:01 +0300

    Add files via upload

---
+++ projact.sql
@@ -0,0 +1,47 @@
+CREATE TABLE adress
+(
+  adressID INT NOT NULL,
+  city_name VARCHAR(20) NOT NULL,
+  street_name VARCHAR(20) NOT NULL,
+  building_number NUMERIC(3) NOT NULL,
+  floor_number NUMERIC(2) NOT NULL,
+  apartment_number NUMERIC(4) NOT NULL,
+  postal_code NUMERIC(10) NOT NULL,
+  PRIMARY KEY (adressID)
+);
+
+CREATE TABLE payment
+(
+  credit_number NUMERIC(16) NOT NULL,
+  expiration_date DATE NOT NULL,
+  security_code NUMERIC(4) NOT NULL,
+  PRIMARY KEY (credit_number)
+);
+
+CREATE TABLE user
+(
+  userName VARCHAR(20) NOT NULL,
+  first_name VARCHAR(20) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+  phone_number NUMERIC(10) NOT NULL,
+  password VARCHAR(6) NOT NULL,
+  mail_adress VARCHAR(30) NOT NULL,
+  URL_website VARCHAR(30) NOT NULL,
+  role VARCHAR(20) NOT NULL,
+  adressID INT NOT NULL,
+  credit_number NUMERIC(16),
+  PRIMARY KEY (userName),
+  FOREIGN KEY (adressID) REFERENCES adress(adressID),
+  FOREIGN KEY (credit_number) REFERENCES payment(credit_number)
+);
+[A###################]
+FirstName
+LastName
+[9999999999]
+[######]
+Email
+'https://' + [A#######] + '.com'
+List('Seller', '')
+ List(select adressID from adress)
+List(select CREDIT_NUMBER from payment)
+
